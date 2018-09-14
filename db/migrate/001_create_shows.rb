733bdab28f076557ec096b4e994a9e33fd052c61
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 20:43:26 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -3,6 +3,8 @@ class CreateShows < ActiveRecord::Migration[5.2]
   def change
     create_table :shows do |t|
       t.string :name
+      t.string :na
+
       t.float :price
       t.string :size
       t.string :image_url
