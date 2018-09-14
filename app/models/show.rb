3e20ca1866afad8e0e1a80a983af9561f621f609
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 21:01:47 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,10 +1,10 @@
 class Show < ActiveRecord::Base
 
-  def highest_rating
+  def self.highest_rating
     Show.maximum(:rating)
   end
 
-  def lowest_rating
+  def self.lowest_rating
     Show.minimum(:rating)
   end
 
